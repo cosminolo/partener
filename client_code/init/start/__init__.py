5883190955a000176d2006887c9d6a2c57702240
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2022-06-10 10:51:08 +0000

    Edited init.start

--- client_code/init/start/__init__.py
+++ client_code/init/start/__init__.py
@@ -33,4 +33,27 @@ class start(startTemplate):
     self.drop_down_2.items = self.drop_down_2.items
     pass
 
+  def drop_down_2_change(self, **event_args):
+    """This method is called when an item is selected"""
+    Linie de Credit - acordare/suplimentare
+    Linie de Credit - prelungire
+    Linie de Credit - suplimentare 10%
+    Descoperit de Cont pe Card Business - Client Nou, cu expunere sub 6 luni
+    Descoperit de Cont pe Card Business - Client Existent, cu expunere de peste 6 luni
+    Visa Business
+    Credit pentru finantarea activitatii curente
+    Credit pentru achitarea datoriilor la buget
+    Credit ipotecar pentru investitii acordat PJ
+    Credit de investitii
+    Credit de investitii pentru achizia de teren pentru productia agricola
+    Credit pentru finantarea activitatii curente OUG 43/2013
+    Credit pentru finantarea investitiilor in domeniul agricol OUG 43/2013
+    Credit pentru achizitia terenurilor cu destinatie agricola OUG 43/2013
+    Activitate curenta agricultura 500 mii-FGCR
+    Investitii in agricultura 500 mii-FGCR
+    Activitate curenta si investitii in agricultura 500 mii-FGCR
+
+    pass
+
+
 
