6818454e7e907c6ec5af37689266ec11e870858a
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2022-06-10 15:34:16 +0000

    Edited settings and init.start

--- client_code/init/start/__init__.py
+++ client_code/init/start/__init__.py
@@ -22,38 +22,77 @@ class start(startTemplate):
     pass
   def con_drop1(self):
     self.drop_down_1.items = []
-    for art in ['S.R.L.', 'S.A', 'PFA', 'II', 'SRL-D']:
+    for art in [' ', 'S.R.L.', 'S.A', 'PFA', 'II', 'SRL-D']:
       self.drop_down_1.items.append(art)
       self.drop_down_1.items = self.drop_down_1.items
     pass
   def con_drop2(self):
     self.drop_down_2.items = []
-    for art in ['negarantat', 'garantat doar FGCR pt act de productie', 'garantat cu echipamente/imobile/etc', 'garantat cu mix (fond si alte garantii)']:
+    for art in [' ','negarantat', 'garantat doar FGCR pt act de productie', 'garantat cu echipamente/imobile/etc', 'garantat cu mix (fond si alte garantii)']:
       self.drop_down_2.items.append(art)
     self.drop_down_2.items = self.drop_down_2.items
     pass
 
   def drop_down_2_change(self, **event_args):
-    """This method is called when an item is selected"""
-    Linie de Credit - acordare/suplimentare
-    Linie de Credit - prelungire
-    Linie de Credit - suplimentare 10%
-    Descoperit de Cont pe Card Business - Client Nou, cu expunere sub 6 luni
-    Descoperit de Cont pe Card Business - Client Existent, cu expunere de peste 6 luni
-    Visa Business
-    Credit pentru finantarea activitatii curente
-    Credit pentru achitarea datoriilor la buget
-    Credit ipotecar pentru investitii acordat PJ
-    Credit de investitii
-    Credit de investitii pentru achizia de teren pentru productia agricola
-    Credit pentru finantarea activitatii curente OUG 43/2013
-    Credit pentru finantarea investitiilor in domeniul agricol OUG 43/2013
-    Credit pentru achizitia terenurilor cu destinatie agricola OUG 43/2013
-    Activitate curenta agricultura 500 mii-FGCR
-    Investitii in agricultura 500 mii-FGCR
-    Activitate curenta si investitii in agricultura 500 mii-FGCR
-
+    if self.drop_down_2.selected_value == 'garantat cu echipamente/imobile/etc' or self.drop_down_2.selected_value == 'garantat cu mix (fond si alte garantii)':
+       self.drop_down_3.items = []
+       for art in [' ', 'Linie de Credit - acordare/suplimentare', 
+                'Linie de Credit - acordare/suplimentare',
+                'Linie de Credit - prelungire',
+                'Linie de Credit - suplimentare 10%',
+                'Descoperit de Cont pe Card Business - Client Nou, cu expunere sub 6 luni',
+                'Descoperit de Cont pe Card Business - Client Existent, cu expunere de peste 6 luni',
+                'Visa Business',
+                'Credit pentru finantarea activitatii curente',
+                'Credit pentru achitarea datoriilor la buget',
+                'Credit ipotecar pentru investitii acordat PJ',
+                'Credit de investitii',
+                'Credit de investitii pentru achizia de teren pentru productia agricola',
+                'Credit pentru finantarea activitatii curente OUG 43/2013',
+                'Credit pentru finantarea investitiilor in domeniul agricol OUG 43/2013',
+                'Credit pentru achizitia terenurilor cu destinatie agricola OUG 43/2013',
+                'Activitate curenta agricultura 500 mii-FGCR',
+                'Investitii in agricultura 500 mii-FGCR',
+                'Activitate curenta si investitii in agricultura 500 mii-FGCR']:
+                self.drop_down_3.items.append(art)
+       self.drop_down_3.items = self.drop_down_3.items
     pass
+
+  def drop_down_1_change(self, **event_args):
+    
+    
+    
+    
+    
+    pass
+
+  def drop_down_2_show(self, **event_args):
+    if self.drop_down_2.item == 'garantat cu echipamente/imobile/etc' or self.drop_down_2.item == 'garantat cu mix (fond si alte garantii)':
+       self.drop_down_3.items = []
+       for art in [' ', 'Linie de Credit - acordare/suplimentare', 
+                'Linie de Credit - acordare/suplimentare',
+                'Linie de Credit - prelungire',
+                'Linie de Credit - suplimentare 10%',
+                'Descoperit de Cont pe Card Business - Client Nou, cu expunere sub 6 luni',
+                'Descoperit de Cont pe Card Business - Client Existent, cu expunere de peste 6 luni',
+                'Visa Business',
+                'Credit pentru finantarea activitatii curente',
+                'Credit pentru achitarea datoriilor la buget',
+                'Credit ipotecar pentru investitii acordat PJ',
+                'Credit de investitii',
+                'Credit de investitii pentru achizia de teren pentru productia agricola',
+                'Credit pentru finantarea activitatii curente OUG 43/2013',
+                'Credit pentru finantarea investitiilor in domeniul agricol OUG 43/2013',
+                'Credit pentru achizitia terenurilor cu destinatie agricola OUG 43/2013',
+                'Activitate curenta agricultura 500 mii-FGCR',
+                'Investitii in agricultura 500 mii-FGCR',
+                'Activitate curenta si investitii in agricultura 500 mii-FGCR']:
+                self.drop_down_3.items.append(art)
+       self.drop_down_3.items = self.drop_down_3.items
+    
+    pass
+
+
 
 
 
