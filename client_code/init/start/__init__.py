fb35e668b616d508d9e38cb974f623fc23b6cf56
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2022-06-10 16:48:56 +0000

    Edited init.start; Deleted init.Form4

--- client_code/init/start/__init__.py
+++ client_code/init/start/__init__.py
@@ -13,13 +13,7 @@ class start(startTemplate):
     self.init_components(**properties)
     self.con_drop1()
     self.con_drop2()
-
-    
-
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('init')
-    pass
+    self.con_drop4()
   def con_drop1(self):
     self.drop_down_1.items = []
     for art in [' ', 'S.R.L.', 'S.A', 'PFA', 'II', 'SRL-D']:
@@ -28,9 +22,15 @@ class start(startTemplate):
     pass
   def con_drop2(self):
     self.drop_down_2.items = []
-    for art in [' ','negarantat', 'garantat doar FGCR pt act de productie', 'garantat cu echipamente/imobile/etc', 'garantat cu mix (fond si alte garantii)']:
+    for art in [' ','negarantat', 'valoare mica cu FGCR', 'OUG 43',  'garantat cu echipamente/imobile/etc', 'garantat cu mix (fond si alte garantii)']:
       self.drop_down_2.items.append(art)
     self.drop_down_2.items = self.drop_down_2.items
+    pass
+  def con_drop4(self):
+    self.drop_down_4.items = []
+    for art in [' ','Lei', 'Eur', 'Usd']:
+      self.drop_down_4.items.append(art)
+    self.drop_down_4.items = self.drop_down_4.items
     pass
 
   def drop_down_2_change(self, **event_args):
@@ -40,59 +40,42 @@ class start(startTemplate):
                 'Linie de Credit - acordare/suplimentare',
                 'Linie de Credit - prelungire',
                 'Linie de Credit - suplimentare 10%',
-                'Descoperit de Cont pe Card Business - Client Nou, cu expunere sub 6 luni',
-                'Descoperit de Cont pe Card Business - Client Existent, cu expunere de peste 6 luni',
-                'Visa Business',
                 'Credit pentru finantarea activitatii curente',
                 'Credit pentru achitarea datoriilor la buget',
                 'Credit ipotecar pentru investitii acordat PJ',
                 'Credit de investitii',
-                'Credit de investitii pentru achizia de teren pentru productia agricola',
-                'Credit pentru finantarea activitatii curente OUG 43/2013',
-                'Credit pentru finantarea investitiilor in domeniul agricol OUG 43/2013',
-                'Credit pentru achizitia terenurilor cu destinatie agricola OUG 43/2013',
-                'Activitate curenta agricultura 500 mii-FGCR',
-                'Investitii in agricultura 500 mii-FGCR',
-                'Activitate curenta si investitii in agricultura 500 mii-FGCR']:
+                'Credit de investitii pentru achizia de teren pentru productia agricola']:
                 self.drop_down_3.items.append(art)
        self.drop_down_3.items = self.drop_down_3.items
-    pass
-
-  def drop_down_1_change(self, **event_args):
-    
-    
-    
-    
-    
-    pass
-
-  def drop_down_2_show(self, **event_args):
-    if self.drop_down_2.item == 'garantat cu echipamente/imobile/etc' or self.drop_down_2.item == 'garantat cu mix (fond si alte garantii)':
-       self.drop_down_3.items = []
-       for art in [' ', 'Linie de Credit - acordare/suplimentare', 
-                'Linie de Credit - acordare/suplimentare',
-                'Linie de Credit - prelungire',
-                'Linie de Credit - suplimentare 10%',
-                'Descoperit de Cont pe Card Business - Client Nou, cu expunere sub 6 luni',
+    if self.drop_down_2.selected_value == 'negarantat':
+      self.drop_down_3.items = []
+      for art in [' ', 'Descoperit de Cont pe Card Business - Client Nou, cu expunere sub 6 luni', 
                 'Descoperit de Cont pe Card Business - Client Existent, cu expunere de peste 6 luni',
-                'Visa Business',
-                'Credit pentru finantarea activitatii curente',
-                'Credit pentru achitarea datoriilor la buget',
-                'Credit ipotecar pentru investitii acordat PJ',
-                'Credit de investitii',
-                'Credit de investitii pentru achizia de teren pentru productia agricola',
-                'Credit pentru finantarea activitatii curente OUG 43/2013',
-                'Credit pentru finantarea investitiilor in domeniul agricol OUG 43/2013',
-                'Credit pentru achizitia terenurilor cu destinatie agricola OUG 43/2013',
-                'Activitate curenta agricultura 500 mii-FGCR',
-                'Investitii in agricultura 500 mii-FGCR',
-                'Activitate curenta si investitii in agricultura 500 mii-FGCR']:
+                'Visa Business'] :
                 self.drop_down_3.items.append(art)
-       self.drop_down_3.items = self.drop_down_3.items
-    
+      self.drop_down_3.items = self.drop_down_3.items
+      
+    if self.drop_down_2.selected_value == 'valoare mica cu FGCR':
+      self.drop_down_3.items = []
+      for art in [' ', 'Activitate curenta agricultura 500 mii-FGCR', 
+                'Investitii in agricultura 500 mii-FGCR',
+                'Activitate curenta si investitii in agricultura 500 mii-FGCR'] :
+                self.drop_down_3.items.append(art)
+      self.drop_down_3.items = self.drop_down_3.items  
+    if self.drop_down_2.selected_value == 'OUG 43':
+      self.drop_down_3.items = []
+      for art in [' ', 'Credit pentru finantarea activitatii curente OUG 43/2013', 
+                'Credit pentru finantarea investitiilor in domeniul agricol OUG 43/2013',
+                'Credit pentru achizitia terenurilor cu destinatie agricola OUG 43/2013'] :
+                self.drop_down_3.items.append(art)
+      self.drop_down_3.items = self.drop_down_3.items     
+      
+      
     pass
 
+    # Any code you write here will run when the form opens.
 
-
-
+  def button_1_click(self, **event_args):
+    open_form('init')
+    pass
 
