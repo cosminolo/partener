d612c495bf5fc856b3429334bde2c6e0eff3f908
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2022-06-15 16:35:58 +0000

    Created Form1; Edited settings; Deleted init.open_h

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import open_hTemplate
+from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,9 +8,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class open_h(open_hTemplate):
+class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
+
     # Any code you write here will run when the form opens.
