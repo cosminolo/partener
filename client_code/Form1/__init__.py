34c817b0146c69d213d48a9a7b2fa2bec1738769
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2022-06-20 08:42:02 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,17 +10,53 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from ..init.start import start
+import json
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     
-    # Any code you write here will run when the form opens.
-
-  def button_1_click(self, **event_args):
-   #form_indv_snake = IndividualSnake(snake=app_tables.snakes.get(name=self.item['name']))
-    new_form = start()
     self.column_panel_1.clear()
-    self.column_panel_1.add_component(new_form)
+   
+    global txb
+    txb = self.txb = {}
+    global i
+    global j
+    global fin
+    i = 1
+    j = 0   
+    fin = {"cont": 231}
+    self.item = fin
+    for i in range(1, 9): # col 
+      k=10+i  
+      if i == 1:
+       self.txb[k] = TextBox(type="text", font="Arial", font_size="10",
+                              spacing_above = "small",
+                              spacing_below = "none",
+                              width=80,
+                              foreground="#000",background="#fff",placeholder=f"place{k}")
+       self.txb[k].role = "input" #"form-control"
+       self.txb[k].tag.name = k
+       self.txb[k].text = self.item["cont"]   
+       self.column_panel_1.add_component(self.txb[k], row=1, col_xs=0, width_xs=1)
+       self.txb[k].set_event_handler('lost_focus', self.l_focus) 
+    
+    
+  def l_focus (self, sender,**event_args):
+    print(self.item)
+    print(sender.tag.name)   
+    fin = json.loads(json.dumps(self.item))
+    print (fin)
+    if not sender.text:
+      pass
+    else:
+      S = sender.text
+      if S.isdigit() or (S.startswith("-") and S[1:].isdigit()):
+        fin = json.loads(json.dumps(self.item))
+        print (fin)
+        pass
+      else:         
+        sender.text = 0     
+        alert("Just numbers without decimal")        
     pass
 
