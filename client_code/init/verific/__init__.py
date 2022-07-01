0e09f9acf2811441332712c94eab0c035a8e6430
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2022-07-01 09:45:17 +0000

    Edited init.verific

--- client_code/init/verific/__init__.py
+++ client_code/init/verific/__init__.py
@@ -266,8 +266,8 @@ class verific(verificTemplate):
   def button_9_click(self, **event_args):
     """This method is called when the button is clicked"""
     lit = anvil.server.call("litigii", "sonimpex")
-    print(lit)
-    self.repeating_panel_1.items = lit
+    print(json.dumps(lit))
+    self.repeating_panel_1.items = json.dumps(lit)
     
     pass
 
