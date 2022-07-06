0c815eaab39ee2538eea74f0891d927c4c64ee74
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2022-07-06 19:56:46 +0000

    Edited init.st

--- client_code/init/st/__init__.py
+++ client_code/init/st/__init__.py
@@ -13,29 +13,23 @@ class st(stTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    p_ini = anvil.server.call("pri_ini")
     global p1
-    p1=p_ini['gen']
-    self.text_box_1.text=p1["cui"]
-    p1["cui"] = self.text_box_1.text  
-    self.text_box_2.text=p1["name"]
-    p1["name"] = self.text_box_2.text  
-    self.text_box_3.text=p1["codj"]
-    p1["codj"] = self.text_box_3.text
-    self.text_box_4.text=p1["adress"]
-    p1["adress"] = self.text_box_4.text
-    self.text_box_5.text=p1["tel"]
-    p1["tel"] = self.text_box_5.text
-    self.text_box_6.text=p1["email"]
-    p1["email"] = self.text_box_6.text
-    self.text_box_7.text=p1["caen"]
-    p1["caen"] = self.text_box_7.text
-    self.text_box_8.text=p1["d_caen"]
-    p1["d_caen"] = self.text_box_8.text
-    self.con_drop1()
-    self.drop_down_1.selected_value = p1["tip"] 
+    p1 = anvil.server.call("pri_ini")
+    self.text_box_1.text=p1['gen']['cui']    
+    self.text_box_2.text=p1['gen']['name'] 
     
-  
+    self.text_box_3.text=p1['gen']['codj']   
+    self.text_box_4.text=p1['gen']['adress']  
+    self.text_box_5.text=p1['gen']['tel']   
+    self.text_box_6.text=p1['gen']['email']
+    self.text_box_7.text=p1['gen']['caen'] 
+    self.text_box_8.text=p1['gen']['d_caen']    
+    self.con_drop1()
+    self.drop_down_1.selected_value = p1['gen']['tip']
+       
+    self.text_area_1.text= p1['asoc_pf']
+    p1['asoc_pf'] = self.text_area_1.text
+    
   def con_drop1(self):
     self.drop_down_1.items = []
     for art in [' ', 'S.R.L.', 'S.A', 'PFA', 'II', 'SRL-D']:
@@ -44,26 +38,36 @@ class st(stTemplate):
 
   def text_box_1_pressed_enter(self, **event_args):
     grup = anvil.server.call("gr", self.text_box_1.text)    
-    #p1["cui"]=grup["soc"][0]["cui"]
-    self.text_box_2.text=grup["soc"][0]["name"]
+    p1['gen']['cui'] = self.text_box_1.text
+    self.text_box_2.text=grup["soc"][0]["name"]    
+    p1['gen']['name'] = self.text_box_2.text
     self.text_box_3.text=grup["soc"][0]["codj"]
     self.text_box_4.text=grup["soc"][0]["adress"]
     self.text_box_5.text=grup["soc"][0]["tel"]
     self.text_box_6.text=grup["soc"][0]["email"]
     self.text_box_7.text=grup["soc"][0]["caen"]
     self.text_box_8.text=grup["soc"][0]["d_caen"]
+    te = ""
+    for i in range(0, len(grup["asoc_pf"])):
+      te = str(te) + str(grup["asoc_pf"][i]["nume"]) + "-" + str(grup["asoc_pf"][i]["cota"] +"%; ")               
+    self.text_area_1.text= te
     print(p1)
     pass
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    print(p1)
+    print(p1['gen'])
     pass
 
   def drop_down_1_change(self, **event_args):
     """This method is called when an item is selected"""
-    p1["tip"] = self.drop_down_1.selected_value
+    p1['gen']['tip'] = self.drop_down_1.selected_value
     pass
+
+  def text_box_2_pressed_enter(self, **event_args):
+    p1['gen']['name'] = self.text_box_2.text
+    pass
+
 
 
 
