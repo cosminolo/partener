b1bb95977200cbfc9d361882477f63b048172fd8
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2022-07-09 17:19:36 +0000

    Saved version

--- client_code/init/verific/__init__.py
+++ client_code/init/verific/__init__.py
@@ -17,66 +17,57 @@ import stripe.checkout
 class verific(verificTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-    global num_den
-    re = json.dumps(anvil.server.call("get_jsgen", self.ups()), indent=2)
-    num_den =  (json.loads(re)['den'])
-    self.text_box_1.text = num_den  
-    global get_tva
-    get_tva = anvil.server.call("get_tva", self.ups())
+    self.init_components(**properties) 
+    global p1    
+    p1 = anvil.server.call("get_p1", self.ups())
     global cui
-    cui = get_tva['cui']
-    self.clear_tva()
-    self.tva()    
-    global bill
-    global bil1
-    bill={}
-    bil1={}
-    bill = anvil.server.call("get_bil", self.ups(), 2)
-    bil1 = anvil.server.call("get_bil", self.ups(), 1)
-    self.clear_bilant()
-    self.bilant(bill, bil1)
+    cui = p1['gen']['cui']
+    global den 
+    den = p1['gen']['name']
+    self.text_box_1.text = den
+    self.ref_buget("a") 
+    self.tva("a")
     try:
-      k = anvil.server.call("prel_js_gol", self.ups(),2)
-      self.label_79.text = k['buget']['bs']
-      self.label_80.text = k['buget']['bss']
-      self.label_81.text = k['buget']['bas']
-      self.label_83.text = k['buget']['bass']
-      self.label_87.text = k['buget']['cui']
-      self.label_89.text = k['buget']['total']
-      self.label_90.text = k['buget']['obs']
+        self.ref_lit("a")
     except:
       pass
-    self.ch_pan()
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('init.crit')
-    
+    open_form('init.crit')    
     pass
-
   def button_1_click(self, **event_args):
      open_form('init')
      pass
-
   def button_3_click(self, **event_args):
-     open_form('init.start')
+     open_form('init.st')
      pass
-
-  def tva(self):   
-    try:      
-      self.label_12.text = get_tva['cui']      
-      self.label_13.text = get_tva['scpTVA']
-      self.label_14.text = get_tva['data_inceput_ScpTVA']
-      self.label_15.text = get_tva['statusRO_e_Factura']
-      self.label_16.text = get_tva['statusInactivi']
-      self.label_17.text = get_tva['statusTvaIncasare']
-      self.label_18.text = get_tva['dataInceputTvaInc']
-      self.label_19.text = get_tva['statusSplitTVA']
-      self.label_20.text = get_tva['dataInceputSplitTVA']
-      self.label_21.text = get_tva['mesaj_ScpTVA']
-      self.label_23.text = get_tva['data']
-    except:
-      pass
+  def tva(self, tip):
+    if tip == "b":
+      gt = json.loads(anvil.server.call("is_1", cui))
+      p1['tva']['cui'] = gt['cui']      
+      p1['tva']['scpTVA'] = gt['scpTVA']
+      p1['tva']['data_inceput_ScpTVA'] = gt['data_inceput_ScpTVA']
+      p1['tva']['statusRO_e_Factura'] = gt['statusRO_e_Factura']
+      p1['tva']['statusInactivi'] = gt['statusInactivi']
+      p1['tva']['statusTvaIncasare'] = gt['statusTvaIncasare']
+      p1['tva']['dataInceputTvaInc'] = gt['dataInceputTvaInc']
+      p1['tva']['statusSplitTVA'] = gt['statusSplitTVA']
+      p1['tva']['dataInceputSplitTVA'] = gt['dataInceputSplitTVA']
+      p1['tva']['mesaj_ScpTVA'] = gt['mesaj_ScpTVA']
+      p1['tva']['data'] = gt['data']        
+      anvil.server.call("sp1", self.ups(), p1)
+    self.clear_tva()        
+    self.label_12.text = p1['tva']['cui']      
+    self.label_13.text = p1['tva']['scpTVA']
+    self.label_14.text = p1['tva']['data_inceput_ScpTVA']
+    self.label_15.text = p1['tva']['statusRO_e_Factura']
+    self.label_16.text = p1['tva']['statusInactivi']
+    self.label_17.text = p1['tva']['statusTvaIncasare']
+    self.label_18.text = p1['tva']['dataInceputTvaInc']
+    self.label_19.text = p1['tva']['statusSplitTVA']
+    self.label_20.text = p1['tva']['dataInceputSplitTVA']
+    self.label_21.text = p1['tva']['mesaj_ScpTVA']
+    self.label_23.text = p1['tva']['data']  
     pass
   def clear_tva(self):      
       self.label_12.text = ""
@@ -100,8 +91,7 @@ class verific(verificTemplate):
           return us
       pass
 
-  def bilant(self, a, b):    
-      
+  def bilant(self, a, b):        
       #time.sleep(1)
       try:
         bil = a      
@@ -148,7 +138,6 @@ class verific(verificTemplate):
       except:
         pass
       pass
-   
   def clear_bilant(self):    
         self.label_41.text = ""
         self.label_42.text = ""
@@ -210,56 +199,54 @@ class verific(verificTemplate):
     print (c["result"])
     pass
 
-  def button_4_click(self, **event_args):      
-      get_tva = anvil.server.call("is_1", cui)
-      anvil.server.call("tva_js", self.ups(), get_tva)
-      self.clear_tva()      
-      self.tva()
-      pass
+  def button_4_click(self, **event_args):
+    self.tva("b")      
+    pass
 
   def button_7_click(self, **event_args):
     self.clear_bilant()
     an = str(int(date.today().year))   
-    c = anvil.server.call("is_2", an, cui)
-    anvil.server.call("bil_js", self.ups(), c, 1 ) 
+    c = json.loads(anvil.server.call("is_2", an, cui))
+    #anvil.server.call("bil_js", self.ups(), c, 1 ) 
     
-    ann = ann = str(int(bil1["an"])-1)      
-    d = anvil.server.call("is_2", ann, cui)
-    anvil.server.call("bil_js", self.ups(), d, 2 )        
-    self.bilant(json.loads(d), json.loads(c))
+    ann = str(int(c['an'])-1)      
+    d = json.loads(anvil.server.call("is_2", ann, cui))
+    #anvil.server.call("bil_js", self.ups(), d, 2 )        
+    self.bilant(d, c)
     pass
 
   def button_8_click(self, **event_args):
+    self.ref_buget("b")
+    pass
+  def ref_buget(self, tip):
     self.label_79.text = ""
     self.label_80.text = ""
     self.label_81.text = ""
     self.label_83.text = ""
     self.label_87.text = ""
     self.label_89.text = ""
-    self.label_90.text = ""    
-    us = self.ups()
-    k = anvil.server.call("prel_js_gol", self.ups(),2)
-    den =  k['den']
-    cui = k['cui']
-    rbb = json.loads(anvil.server.call("rest_buget", us, den, cui))
-    
-    if len(rbb)>0:
-      k['buget'].update(rbb)
-    else:      
-      k['buget']['nume'] = den
-      k['buget']['cui'] = cui
-      k['buget']['obs'] = "Nu are restante la finele ultimului trimestru"
-   
-    self.label_79.text = k['buget']['bs']
-    self.label_80.text = k['buget']['bss']
-    self.label_81.text = k['buget']['bas']
-    self.label_83.text = k['buget']['bass']
-    self.label_87.text = k['buget']['cui']
-    self.label_89.text = k['buget']['total']
-    self.label_90.text = k['buget']['obs']
-    anvil.server.call("upp_js", self.ups(), k) 
-    pass
-  
+    self.label_90.text = ""
+    self.label_96.text = ""
+    us = str(self.ups()).strip()
+    if tip == "b":
+      rbb = json.loads(anvil.server.call("rest_buget", us, den, cui))    
+      if len(rbb)>0:
+        p1['buget'].update(rbb)
+        p1['buget']['datav'] = str(date.today().isoformat())
+      else:      
+        p1['buget']['nume'] = den
+        p1['buget']['cui'] = cui
+        p1['buget']['obs'] = "Nu are restante la finele ultimului trimestru"
+        p1['buget']['datav'] = str(date.today().isoformat())
+      anvil.server.call("sp1", self.ups(), p1)
+    self.label_79.text = p1['buget']['bs']
+    self.label_80.text = p1['buget']['bss']
+    self.label_81.text = p1['buget']['bas']
+    self.label_83.text = p1['buget']['bass']
+    self.label_87.text = p1['buget']['cui']
+    self.label_89.text = p1['buget']['total']
+    self.label_90.text = p1['buget']['obs']
+    self.label_96.text = p1['buget']['datav']
   def ps(self, sst):
     s=""
     st=str(sst)
@@ -267,43 +254,46 @@ class verific(verificTemplate):
        if i.isnumeric() or i=="," or i.isalpha():
         s = s + i
     return s
-
   def button_9_click(self, **event_args): 
-    self.drop_down_1.selected_value = ""
-    self.drop_down_2.selected_value = ""
-    den = self.text_box_1.text.strip()
-    li = json.loads(anvil.server.call("litigii", den))
-    #anvil.server.call("s_lit", self.ups(), li)    
-    p1["litigii"].update(li)
-    #lit = json.loads(li)
-    lit = p1["litigii"]
-    cal = []
-    calitate =[""]
-    ob = []
-    obiect=[""]
-    if len(lit) > 1:
-     for i in range(0, len(lit)):
-      cal.append(lit[i]['calitate'])
-      [calitate.append(x) for x in cal if x not in calitate]
-      ob.append(lit[i]['obiect'])
-      [obiect.append(x) for x in ob if x not in obiect]
-     self.drop_down_1.items = calitate
-     self.drop_down_2.items = obiect    
-     self.repeating_panel_1.items = lit
-    else:
-        self.repeating_panel_1.items = []
-        if lit[0]["denumire"] == "nu inregistreaza":
-          n = Notification("",
-             title=lit[0]["denumire"]).show()
-          time.sleep(1)
-          n.hide()
-        else:
-          n = Notification("",
-             title=lit[0]["denumire"]).show()
-          time.sleep(1)
-          n.hide()
+    self.ref_lit("b")
     pass
-
+  def ref_lit(self, tip):    
+      self.drop_down_1.selected_value = ""
+      self.drop_down_2.selected_value = ""
+      den = self.text_box_1.text
+      if tip == "b":
+        li = anvil.server.call("litigii", den)
+        p1['litigii'] = li
+        anvil.server.call("sp1", self.ups(), p1)
+      lit = json.loads(p1['litigii'])
+      self.repeating_panel_1.items = lit
+      cal = []
+      calitate =[""]
+      ob = []
+      obiect=[""]
+      if len(lit) > 0:
+        for i in range(0, len(lit)):
+          cal.append(lit[i]['calitate'])
+          [calitate.append(x) for x in cal if x not in calitate]
+          ob.append(lit[i]['obiect'])
+          [obiect.append(x) for x in ob if x not in obiect]
+        self.drop_down_1.items = calitate
+        self.drop_down_2.items = obiect    
+        self.repeating_panel_1.items = lit
+      else:
+        if tip == "b": 
+         self.repeating_panel_1.items = []
+         if lit[0]["denumire"] == "nu inregistreaza":
+          n = Notification("",
+             title=lit[0]["denumire"]).show()
+          time.sleep(1)
+          n.hide()
+         else:
+          n = Notification("",
+             title=lit[0]["denumire"]).show()
+          time.sleep(1)
+          n.hide() 
+      pass
   def drop_down_1_change(self, **event_args):
     """This method is called when an item is selected"""
     if self.drop_down_1.selected_value != "":
@@ -315,7 +305,6 @@ class verific(verificTemplate):
           filt.append(f)
       self.repeating_panel_1.items = filt    
     pass
-
   def drop_down_2_change(self, **event_args):
     """This method is called when an item is selected"""
     if self.drop_down_2.selected_value != "":
@@ -327,26 +316,4 @@ class verific(verificTemplate):
           filt.append(f)
       self.repeating_panel_1.items = filt   
     pass
-  def ch_pan(self):
-    li= anvil.server.call('get_lit', self.ups())
-    
-    try:
-      lit = json.loads(li)
-      if len(lit)>0:
-         self.repeating_panel_1.items = lit
-    except:
-        pass
-    pass
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
