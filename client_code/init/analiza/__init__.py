e102f009b473ac6982d55760f7991207c2a0344f
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2022-11-16 12:13:39 +0000

    Edited init.analiza

--- client_code/init/analiza/__init__.py
+++ client_code/init/analiza/__init__.py
@@ -16,3 +16,8 @@ class analiza(analizaTemplate):
 
     # Any code you write here will run when the form opens.
     
+
+  def text_box_23_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
