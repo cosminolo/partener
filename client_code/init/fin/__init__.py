87c3b0cc4144fb827baaa713cd0bd794219a2f8d
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2022-11-17 14:20:21 +0000

    Edited init.fin

--- client_code/init/fin/__init__.py
+++ client_code/init/fin/__init__.py
@@ -624,6 +624,8 @@ class fin(finTemplate):
 
   def file_loader_2_change(self, file, **event_args):
     """This method is called when a new file is loaded into this FileLoader"""
+    f = file
+    f = anvil.BlobMedia('Balante.xlsx', file.get_bytes())
     anvil.server.call("up_sf", self.ups(), file)
     pass
 
