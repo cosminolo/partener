636a08fc8c6b45a652fade3a4390100d0148b3e9
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2022-12-07 16:12:33 +0000

    Saved version

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -30,7 +30,10 @@ def sentem(us, soc,facilit, dat):
 pass
 @anvil.server.callable
 def clr_ar(id):
-  c1 = json.loads(anvil.server.call("get_crit", id))  
+  try:
+     c1 = json.loads(anvil.server.call("get_crit", id)) 
+  except:
+     c1 = anvil.server.call("get_crit", id) 
   cc = {"ar":{"tip":"", "sum_lei":0, "per":0, "perf":"", "l1":0, "l2":0, "l3":0, "l4":0, "l5":0, "l6":0, "l7":0, "l8":0, "l9den":" nealocat", "l9val":0, "l10den":"nealocat", "l10val":0, "l11den":"nealocat", "l11val":0, "comb_den":"", "comb_val":"",
     "p1":"", "p2":"", "p3":"", "p4":"", "p5":"", "p6":"", "p1r":"", "p2r":"", "p3r":"", "p4r":"", "p5r":"", "p6r":""}} 
   c1["ar"] = cc["ar"]
