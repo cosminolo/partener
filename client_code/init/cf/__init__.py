5c02fbca9aad74573d0baf1bb1248391af1e6060
cosmin.ologeanu2017@gmail.com <cosmin.ologeanu2017@gmail.com>
2023-01-11 15:03:28 +0000

    Saved version

--- client_code/init/cf/__init__.py
+++ client_code/init/cf/__init__.py
@@ -56,6 +56,25 @@ class cf(cfTemplate):
       js_int={}
       js_int["a"]=js1[0]
       js_int["b"]=js1[1]
+      js_int["c"]=js1[2]
+      js_int["d"]=js1[3]
+      js_int["e"]=js1[4]
+      js_int["f"]=js1[5]
+      js_int["g"]=js1[6]
+      js_int["h"]=js1[7]
+      js_int["i"]=js1[8]
+      js_int["j"]=js1[9]
+      js_int["k"]=js1[10]
+      js_int["l"]=js1[11]
+      js_int["m"]=js1[12]
+      js_int["n"]=js1[13]
+      js_int["o"]=js1[14]
+      js_int["p"]=js1[15]
+      js_int["r"]=js1[16]
+      js_int["s"]=js1[17]
+      js_int["t"]=js1[18]
+      js_int["x"]=js1[19]
+      js_int["y"]=js1[20]
     jsf.append(js_int)
     js_int["premise"]=js2
     jsf.append(js_int)
